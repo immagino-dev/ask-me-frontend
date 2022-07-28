fa8f94b847ac71e2833176a8695151c9d53d8b00
Higor Konig <higor.konig@gmail.com>
2022-07-28 19:10:43 -0300

    configurando porta vite

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,12 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 3030
+  },
+  preview: {
+    port: 5000
+  },
   resolve: {
     alias: [
       { find: '~', replacement: path.resolve(__dirname, 'src') },
